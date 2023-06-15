4bbccc349e50cd271cd5200397557973c5ab8243
YoungHyoKim90 <younghyo1101@gmail.com>
2023-06-15 18:08:50 +0900

    23.06.15 python

--- HELLO_PYTHON/Test01/myflask01.py
+++ HELLO_PYTHON/Test01/myflask01.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return redirect("static/omok04_19.html")
+    return redirect("/static/examples/_ex01.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
